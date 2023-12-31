36cfd6f219ab3032fd930e2ff5f7c9fa21fe53ff
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2023-12-31 11:35:40 +0100

    update upload script

--- wc.js
+++ wc.js
@@ -2,11 +2,13 @@ class Upload {
     constructor(label, input) {
         this.label = label;
         this.input = input;
+        this.templateRow = undefined;
         this.init();
     }
     init() {
         this.label.addEventListener("click", () => this.input.click());
         this.label.addEventListener("click", (event) => this.onclick(event));
+        this.templateRow = document.querySelector("tbody .template");
     }
     get files() {
         return this.input.files;
@@ -14,6 +16,7 @@ class Upload {
     onclick(event) {
         console.log(event);
     }
+    addrow({ fileName, fileSize, lineCount, wordCount, byteCount }) {}
 }
 
 const upload = new Upload(
